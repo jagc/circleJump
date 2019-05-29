c72b43cd48d2d717d28f327fd27aa7825f4202dc
jagc <jag.cordoviz@gmail.com>
2019-05-29 14:18:16 +0800

    bugfix: - substituted orbit_position with orbitPosition
    learned:
    1 - When emitting a signal, you can pass data.
    2 - You don't have to manually emit a signal.
    3 - You can just recognize an event as a signal and treat it as one.
    4 - The script receiving the signal receives the whole object

--- scripts/jumper.gd
+++ scripts/jumper.gd
@@ -12,18 +12,13 @@ func jump():
 	target = null
 	velocity = transform.x * jumpSpeed
 
+# 'area' is the circle.gd object
 func _on_jumper_area_entered(area):
 	target = area
 	velocity = Vector2()
 
 func _physics_process(delta):
 	if target:
-#		for i in len(target):
-#			print(target[i])
-#			print(target)
-#			print(target.position)
-#		transform = target.orbit_position.global_transform
-		transform = target.position.transform
-#		transform = target.position.global_transform
+		transform = target.orbitPosition.global_transform
 	else:
 		position += velocity * delta
