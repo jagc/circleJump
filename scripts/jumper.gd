f5614479eacea94796294cd39b8285f62bc0dc30
jagc <jag.cordoviz@gmail.com>
2019-05-30 17:39:48 +0800

    modified capability of trail - while rotating in a circle, trail is offset from the jumper. - while jumping, trail is dead center to the position of the jumper.

--- scripts/jumper.gd
+++ scripts/jumper.gd
@@ -8,12 +8,15 @@ var velocity = Vector2(100, 0) # start value for testing
 var jumpSpeed = 1000
 var target = null # if we're on a circle
 var trailLength = 25
+var is_rotatingClockwise = 1
+var isJumping = false
 
 func _unhandled_input(event):
 	if target and event is InputEventScreenTouch and event.pressed:
 		jump()
 
 func jump():
+	isJumping = true
 	target.implode()
 	target = null
 	velocity = transform.x * jumpSpeed
@@ -21,9 +24,10 @@ func jump():
 # 'area' is the circle.gd object
 func _on_jumper_area_entered(area):
 	target = area
+	isJumping = false
 
 	var _rotation = (position - target.position).angle()
-	var is_rotatingClockwise = 1 if _rotation > 1.4 else -1
+	is_rotatingClockwise = 1 if _rotation > 1.4 else -1
 
 	target.get_node("pivot").rotation = _rotation
 	velocity = Vector2.ZERO
@@ -33,7 +37,15 @@ func _on_jumper_area_entered(area):
 func _physics_process(delta):
 	if trail.points.size() > trailLength:
 		trail.remove_point(0)
-	trail.add_point(position)
+
+	if isJumping == false:
+		if is_rotatingClockwise == 1:
+			trail.add_point(position + Vector2(0,15).rotated(rotation))
+		else:
+			trail.add_point(position - Vector2(0,15).rotated(rotation))
+	else:
+		trail.add_point(position)
+	
 	
 	if target:
 		transform = target.orbitPosition.global_transform
