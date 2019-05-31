4f0a56c9ca6efbe7df58a8bc114ab8d3b58a31cc
jagc <jag.cordoviz@gmail.com>
2019-05-31 16:29:44 +0800

    made buttons on settings screen functional

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -2,6 +2,10 @@ extends Node
 
 signal startGame
 
+var soundButtons = {true: preload("res://assets/images/buttons/audioOn.png"),
+					false: preload("res://assets/images/buttons/audioOff.png")}
+var musicButtons = {true: preload("res://assets/images/buttons/musicOn.png"),
+					false: preload("res://assets/images/buttons/musicOff.png")}
 var currentScreen = null
 
 func _ready():
@@ -11,12 +15,12 @@ func _ready():
 func registerButtons():
 	var buttons = get_tree().get_nodes_in_group("buttons")
 	for button in buttons:
-		button.connect("pressed", self, "_on_button_pressed", [button.name])
+		button.connect("pressed", self, "_on_button_pressed", [button])
 		
-func _on_button_pressed(name):
+func _on_button_pressed(button):
 	if settings.enableSound:
 		$click.play()
-	match name:
+	match button.name:
 		"home":
 			changeScreen($titleScreen)
 		"play":
@@ -25,6 +29,12 @@ func _on_button_pressed(name):
 			emit_signal("startGame")
 		"settings":
 			changeScreen($settingsScreen)
+		"sound":
+			settings.enableSound = !settings.enableSound
+			button.texture_normal = soundButtons[settings.enableSound]
+		"music":
+			settings.enableMusic = !settings.enableMusic
+			button.texture_normal = musicButtons[settings.enableMusic]
 
 func changeScreen(newScreen):
 	if currentScreen:
